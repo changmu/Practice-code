d17919f305062c7affe0a2574bffc081a21e9f54
changmu <2276479303@qq.com>
2016-05-06 19:39:15 -0700

    update

--- TLPI/alarm.c
+++ TLPI/alarm.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
     int counter;
     alarm(1);
     for (counter = 0; 1; ++counter)
-        printf("counter = %d ", counter);
+        printf("counter = %d\n", counter);
     return 0;
 }
