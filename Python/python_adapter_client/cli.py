788afbd310840c1f494e8058fb6ecae65fd889ab
heurei <2276479303@qq.com>
2016-11-05 11:05:35 +0800

    添加python网络编程example

---
+++ Python/python_adapter_client/cli.py
@@ -0,0 +1,41 @@
+import socket
+import struct
+import time
+
+s = socket.socket()
+s.connect(("10.0.0.92", 7070))
+
+pkt_length = 14
+pkt_type = 234
+pkt_recv_user_id = 2
+pkt_send_user_id = 1
+
+pkt = struct.pack("<IHII", pkt_length, pkt_type, pkt_recv_user_id, pkt_send_user_id)
+
+s.send(pkt)
+time.sleep(1)
+
+pkt_length = 1000
+pkt_type = 200
+pkt_recv_user_id = 2
+pkt_send_user_id = 1
+data = "abcdefg"
+pkt = struct.pack("<IHII986s", pkt_length, pkt_type, pkt_recv_user_id, pkt_send_user_id, data)
+
+pkt_all_cnt = 1000000
+pkt_ok_cnt = 0
+cnt = 0
+
+for i in range(0, pkt_all_cnt):
+    # if cnt == 100:
+        # cnt = 0
+        # time.sleep(0.001)
+
+    ret = s.send(pkt)
+    if ret == len(pkt):
+        pkt_ok_cnt += 1
+
+    cnt += 1
+
+print "ok/all %d/%d" % (pkt_ok_cnt, pkt_all_cnt)
+
---
+++ Python/python_adapter_client/srv.py
@@ -0,0 +1,32 @@
+import socket
+import struct
+import time
+import signal
+import sys
+
+pkt_recv = 0
+
+def sighandler(sig, frame):
+    if (sig == signal.SIGINT):
+        print "recv %d pkts" % (pkt_recv)
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, sighandler)
+
+s = socket.socket()
+s.connect(("10.0.0.163", 7070))
+
+pkt_length = 14
+pkt_type = 234
+pkt_recv_user_id = 1
+pkt_send_user_id = 2
+
+pkt = struct.pack("<IHII", pkt_length, pkt_type, pkt_recv_user_id, pkt_send_user_id)
+
+s.send(pkt)
+time.sleep(1)
+
+while (1):
+    ret = s.recv(1000)
+    pkt_recv += 1
+
