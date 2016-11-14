8fa71ddf0686702b71e897075fc2f53d7cd68383
heurei <2276479303@qq.com>
2016-11-14 20:34:00 +0800

    update

--- C_C++/getpeername.cpp
+++ C_C++/getpeername.cpp
@@ -10,34 +10,48 @@
 #include <strings.h>
 #include <sys/epoll.h>
 
+#define FILENAME "./ip.txt"
 #define BLOCK_SIZE 10240
-char buf[BLOCK_SIZE];
+#define NO_EINTR(stmt) while ((stmt) == -1 && errno == EINTR);
 
 int main(int argc, char *argv[])
 {
-    int sock;
+    // daemon(1, 1);
+    char buf[BLOCK_SIZE] = { 0 };
+    
+    int listenFd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(listenFd != -1);
+
+    int flag = 1;
+    int len = sizeof(flag);
+    int ret = setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &flag, len);
+    assert(ret != -1);
+
     struct sockaddr_in addr;
     bzero(&addr, sizeof(addr));
     addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("0.0.0.0");
     addr.sin_port = htons(8086);
-    addr.sin_addr.s_addr = inet_addr("115.159.54.250");
-
     socklen_t addr_len = sizeof(addr);
-    bool isConnected = false;
-    int ret;
 
+    ret = bind(listenFd, (struct sockaddr *) &addr, addr_len);
+    assert(ret != -1);
+
+    listen(listenFd, 5);
+
+    int sock;
+    sockaddr_in peerAddr;
     while (1) {
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        ret = connect(sock, (struct sockaddr *) &addr, addr_len);
-        if (ret == -1) {
-            fprintf(stderr, "connecting remote host...\n");
-            sleep(3);
-        } else {
-            isConnected = true;
-        }
+        sock = accept(listenFd, (struct sockaddr *) &addr, &addr_len);
 
-        ret = getpeername(sock, (struct sockaddr *) &addr, &addr_len);
-        printf("peer ip: %s\n", inet_ntoa(addr.sin_addr));
+        static __thread char buf[40];
+        struct sockaddr_in addr;
+        socklen_t addr_len = sizeof(addr);
+
+        int ret = getpeername(sock, (struct sockaddr *) &addr, &addr_len);
+        inet_ntop(AF_INET, &addr.sin_addr, buf, addr_len);
+        printf("client ip: %s\n", buf);
+        sleep(1);
     }
 
     return 0;
