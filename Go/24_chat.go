4a00249fbd3bb0a83f14a9d8c2e05ff9809d1848
zhenggang.qiu <changmuqiu@gmail.com>
2018-11-19 10:15:56 +0800

    add chat srv of golang code

---
+++ Go/24_chat.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+    "net"
+    "log"
+    "bufio"
+    "fmt"
+)
+
+func main() {
+    listenAddr := "localhost:8080"
+    listener, err := net.Listen("tcp", listenAddr)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Print("listening ", listenAddr)
+
+    go broadcaster()
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Print(err)
+            continue
+        }
+
+        go handleConn(conn)
+    }
+}
+
+type client chan<-string
+var (
+    entering = make(chan client)
+    leaving = make(chan client)
+    messages = make(chan string) // 所有接收的客户消息
+)
+
+func broadcaster() {
+    clients := make(map[client]bool)
+    for {
+        select {
+        case msg := <-messages:
+            for cli := range clients {
+                cli <- msg
+            }
+        case cli := <-entering:
+            clients[cli] = true
+        case cli := <-leaving:
+            delete(clients, cli)
+            close(cli)
+        }
+
+    }
+}
+
+func handleConn(conn net.Conn) {
+    ch := make(chan string)
+    go connWriter(conn, ch)
+
+    who := conn.RemoteAddr().String()
+
+    ch <- "You are " + who
+
+    messages <- who + " has arrived."
+    entering <- ch
+
+    input := bufio.NewScanner(conn)
+    for input.Scan() {
+        messages <- who + ": " + input.Text()
+    }
+
+    leaving <- ch
+    messages <- who + " has left."
+
+    conn.Close()
+}
+
+func connWriter(conn net.Conn, ch <-chan string) {
+    for msg := range ch {
+        fmt.Fprintln(conn, msg)
+    }
+}
