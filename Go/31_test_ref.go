539372e5a4479dbb65a9e6cefca170fcaef0fa26
zhenggang.qiu <changmuqiu@gmail.com>
2018-12-20 19:13:23 +0800

    add golang code

---
+++ Go/31_test_ref.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := []int{
+		1, 2, 3,
+	}
+    fmt.Println(s)
+
+    f0(s)
+
+	fmt.Println(s)
+
+    f1 := func() {
+        s[0] *= 10
+        s = append(s, 4)
+    }
+    f1()
+
+    fmt.Println(s)
+}
+
+func f0(s []int) {
+    s[0] *= 10
+    s = append(s, 4)
+}
