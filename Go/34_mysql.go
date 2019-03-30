4497ae1bb5b8d235cbb5185c3d263412aef3d61f
qiuzhenggang <qiuzhenggang@wezhuiyi.com>
2019-03-30 23:01:49 +0800

    add

--- Go/34_mysql.go
+++ Go/34_mysql.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "qiu:@tcp(192.168.126.10:3306)/?charset=utf8")
+	// db, err := sql.Open("mysql", "qiu:@tcp(192.168.126.10:3306)/?charset=utf8")
+	db, err := sql.Open("sqlite3", "./sqlite.db")
 	checkError(err)
 
 	// insert
@@ -32,6 +34,24 @@ func main() {
 	checkError(err)
 	fmt.Println("affect =", affect)
 
+	// query
+	rows, err := db.Query("SELECT * FROM test.userinfo")
+	checkError(err)
+
+	for rows.Next() {
+		uid, username, department, created := 0, "", "", ""
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkError(err)
+
+		fmt.Printf("row: %v,%v,%v,%v\n", uid, username, department, created)
+	}
+
+	// delete
+	stmt, err = db.Prepare("DELETE FROM test.userinfo where uid>?")
+	checkError(err)
+
+	res, err = stmt.Exec(3)
+	// fmt.Printf("res=%v\n", res)
 }
 
 func checkError(err error) {
