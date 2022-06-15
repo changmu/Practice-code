f504f8852bb236600fc92f39c49b0dcaedc1ee5f
windqiu <changmuqiu@gmail.com>
2022-06-15 15:12:28 +0800

    learn js

--- NodeJs/04.ContinueTag/index.js
+++ NodeJs/04.ContinueTag/index.js
@@ -3,8 +3,22 @@
 let log = console.log;
 let alert = log;
 
-function showMenu({ title = "Menu", width = 100, height = 200 }={}) {
-    alert( `${title} ${width} ${height}` );
-}
+
+function User(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
   
-showMenu({}); // Menu 100 200
+    // 年龄是根据当前日期和生日计算得出的
+    Object.defineProperty(this, "age", {
+      get() {
+        let todayYear = new Date().getFullYear();
+        return todayYear - this.birthday.getFullYear();
+      }
+    });
+  }
+  
+  let john = new User("John", new Date(1992, 6, 1));
+  
+  alert( john.birthday ); // birthday 是可访问的
+  alert( john.age );      // ……age 也是可访问的
+  log(Object.getOwnPropertyDescriptor(john, "age"))
