40bd8f22d692afd31cb91bc77ccf6341d64b044e
windqiu <changmuqiu@gmail.com>
2022-06-19 11:07:35 +0800

    update js

--- NodeJs/04.ContinueTag/index.js
+++ NodeJs/04.ContinueTag/index.js
@@ -4,28 +4,3 @@ let log = console.log;
 let alert = log;
 
 
-
-function* generateSequence(start, end) {
-    for (let i = start; i <= end; i++) yield i;
-  }
-  
-  function* generatePasswordCodes() {
-  
-    // 0..9
-    yield* generateSequence(48, 57);
-  
-    // A..Z
-    yield* generateSequence(65, 90);
-  
-    // a..z
-    yield* generateSequence(97, 122);
-  
-  }
-  
-  let str = '';
-  
-  for(let code of generatePasswordCodes()) {
-    str += String.fromCharCode(code);
-  }
-  
-  alert(str); // 0..9A..Za..z
